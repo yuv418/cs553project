45e189c07e2b2e8bf48eb1914bfd8910966719ea
Ramesh Balaji <me@ramesh.tv>
2025-05-08 15:15:41 -0400

    The automation (mostly) works

--- client-automation/src/input_simulator.py
+++ client-automation/src/input_simulator.py
@@ -6,19 +6,39 @@
 # https://stackoverflow.com/questions/32098110/selenium-webdriver-java-need-to-send-space-keypress-to-the-website-as-whol
 # https://stackoverflow.com/questions/46361494/how-to-get-the-localstorage-with-python-and-selenium-webdriver
 # https://stackoverflow.com/questions/26566799/wait-until-page-is-loaded-with-selenium-webdriver-for-python - wait (copied)
+# https://stackoverflow.com/questions/14257373/how-to-skip-the-headers-when-processing-a-csv-file-using-python
+# https://smithay.github.io/smithay/smithay/backend/libinput/struct.LibinputInputBackend.html
+# https://stackoverflow.com/questions/62501219/how-to-send-keys-to-a-canvas-element-for-longer-duration
 
 from selenium import webdriver
 import sys
 import os
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+import csv
 
 cwd = os.getcwd()
 sel_folder = os.path.abspath(os.path.join(cwd, "selenium"))
 game_url = os.getenv("GAME_URL")
+input_csv = os.getenv("INPUT_CSV")
+jump_times = []
 
 print(f"sel_folder is {sel_folder}")
+print(f"input_csv is {input_csv}")
+
+with open(input_csv, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)
+    # skip header row
+    for row in reader:
+        jump_times.append(int(row[1]))
+
+# transform jump times
+jump_times = [i - jump_times[0] for i in jump_times]
+print(jump_times)
 
 options = webdriver.ChromeOptions()
 for it in sys.argv[1:]:
@@ -44,3 +64,16 @@ submit = driver.find_element(By.CSS_SELECTOR, ".form-group")
 username.send_keys("admin")
 password.send_keys("password")
 submit.submit()
+
+# turn the input into an action chain.
+chain = ActionChains(driver)
+for i, time in enumerate(jump_times[:-1]):
+    chain.key_down(Keys.SPACE)
+    chain.key_up(Keys.SPACE)
+
+    pause_time = (jump_times[i+1] - time) / 1000
+    print(f"pausing for {pause_time}")
+
+    chain.pause(pause_time)
+
+chain.perform()
